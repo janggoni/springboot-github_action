38511c09d7489098e9a3925038fcbe41f54efcf5
goni <wkdgudrhs@naver.com>
2024-12-14 22:47:15 +0900

    first commit

---
+++ src/main/java/com/example/instagrams_server/InstagramsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.instagrams_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramsServerApplication.class, args);
+	}
+
+}
